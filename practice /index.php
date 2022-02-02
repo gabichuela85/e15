13d3023f0d5f36f79eeb8e65477e0883a7456385
Gaby Brown <gaby.leonguerrero@gmail.com>
2022-02-02 17:55:12 +0000

    Added info to readme. initialized practice app

---
+++ practice /index.php
@@ -0,0 +1,14 @@
+<!doctype html> 
+<html lang='en'>
+
+<head>
+    <title>Practice App</title>
+    <met charset='UTF-8'>
+    <link href=data: , rel=icon>
+</head>
+
+<body> 
+    <h1>Practice App</h1>
+</body>
+
+</html>
