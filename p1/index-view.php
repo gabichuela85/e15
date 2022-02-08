9d8a1e0a081d205ef0a0d8dc0da987564c334766
Gaby Brown <gaby.leonguerrero@gmail.com>
2022-02-08 20:47:32 +0000

    Worked on 3 - string processor and challenge

--- p1/index-view.php
+++ p1/index-view.php
@@ -9,13 +9,15 @@
 
 <body>
     <h1>Project 1 E15</h1>
-    <?php if ($reverse==$inputWord) { ?>
+    <?php if ($reverse==$lowercase) { ?>
     this is a palindrome.
     <?php } else { ?>
     this is not a palindrome.
     <?php } ?>
 
     <p><?php echo($inputWord); ?> has <?php echo($vowelCount); ?> vowels.</p>
+    <p>The coded version of your word is <?php echo($coded); ?>.</p>
+
 </body>
 
 </html>
--- p1/index.php
+++ p1/index.php
@@ -1,6 +1,10 @@
 <?php
 
-$inputWord = 'elle';
+
+$inputWord = 'The zoo is open';
+
+$lowercase = strtolower($inputWord);
+
 $vowels = [
     'a',
     'e',
@@ -9,7 +13,7 @@ $vowels = [
     'u'
 ];
 //this will check to see if the input word is a palindrome
-$reverse = strrev($inputWord);
+$reverse = strrev($lowercase);
 
 //this will count the vowels of the input word
 
@@ -19,4 +23,25 @@ foreach ($vowels as $vowel) {
     $vowelCount += substr_count($inputWord, $vowel);
 }
 
-require 'index-view.php';
+//shift the each letter in the input word one spot
+
+$shifty=['A' => 'B', 'a' => 'b', 'B' => 'C', 'b' => 'c', 'C' => 'D', 'c' => 'd',
+        'D' => 'E', 'd' => 'e', 'E' => 'F', 'e' => 'f', 'F' => 'G', 'f' => 'g',
+        'G' => 'H', 'g' => 'h', 'H' => 'I', 'h' => 'i', 'I' => 'J', 'i' => 'j',
+        'J' => 'K', 'j' => 'k', 'K' => 'L', 'k' => 'l', 'L' => 'M', 'l' => 'm',
+        'M' => 'N', 'm' => 'n', 'N' => 'O', 'n'=> 'o', 'O' => 'P', 'o' => 'p',
+        'P' => 'Q', 'p' => 'q', 'Q' => 'R', 'q' => 'r', 'R' => 'S', 'r' => 's',
+        'S' => 'T', 's' => 't', 'T' => 'U', 't' => 'u', 'U' => 'V', 'u' => 'v',
+        'V' => 'W', 'v' => 'w', 'W' => 'X', 'w' => 'x', 'X' => 'Y', 'x' => 'y',
+        'Y' => 'Z', 'y' => 'z', 'Z' => 'A', 'z' => 'a' ];
+
+$coded = strtr($inputWord, $shifty);
+
+//create an anagram from the word
+
+$anagram = str_shuffle($lowercase);
+var_dump($anagram);
+
+
+
+require 'index-view.php'; 
