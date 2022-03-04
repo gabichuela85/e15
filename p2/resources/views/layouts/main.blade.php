c5956ad5360dd698c85f69becde0eabf79e51fa7
Gaby Brown <gaby.leonguerrero@gmail.com>
2022-03-04 04:27:37 +0000

    project blade templating

---
+++ p2/resources/views/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!doctype hml>
+    <html lang='en'>
+        <head>
+            <title>@yield('title')</title>
+            <meta charset='utf-8'>
+            <link href='/css/movie.css' , rel='stylesheet'>
+
+            @yield('head')
+
+        </head>
+    <body>
+        <section>
+            @yield('content')
+        </section>
+    </body>
+</html>
--- p2/resources/views/welcome.blade.php
+++ p2/resources/views/welcome.blade.php
@@ -1,29 +1,27 @@
-<!doctype hml>
-<html lang='en'>
-<head>
-    <title> </title>
-    <meta charset='utf-8'>
-    <link href=data: , rel=icon>
-</head>
-<body>
-<h1>Movie Reviews</h1>
-<form method='get' action=''>
-    <p></p>
-    <label for='name'>Your Name:</label>
-    <input type='text' name='name' id='name'>
-    <label for='email'>Email:</label>
-    <input type='email' name='email' id='email'>
-    <br/>
-    <span class="star-rating"> 
-        <input type="radio" name="rating1" value="1"><i></i>
-        <input type="radio" name="rating1" value="2"><i></i>
-        <input type="radio" name="rating1" value="3"><i></i>
-        <input type="radio" name="rating1" value="4"><i></i>
-        <input type="radio" name="rating1" value="5"><i></i>
-    </span>
-    <label for='review'>What did you think?</label>
-    <input type='textarea' name='review' id='review'>
-    <button type='submit'>submit</button>
-</form>
-</body>
-</html>
+@extends('layouts/main')
+
+@section('title')
+    Movie Reviews
+@endsection
+
+@section('content')
+    <h1>Movie Reviews</h1>
+    <form method='get' action=''>
+        <p></p>
+        <label for='name'>Your Name:</label>
+        <input type='text' name='name' id='name'>
+        <label for='email'>Email:</label>
+        <input type='email' name='email' id='email'>
+        <br />
+        <span class="star-rating">
+            <input type="radio" name="rating1" value="1"><i></i>
+            <input type="radio" name="rating1" value="2"><i></i>
+            <input type="radio" name="rating1" value="3"><i></i>
+            <input type="radio" name="rating1" value="4"><i></i>
+            <input type="radio" name="rating1" value="5"><i></i>
+        </span>
+        <label for='review'>What did you think?</label>
+        <input type='textarea' name='review' id='review'>
+        <button type='submit'>submit</button>
+    </form>
+@endsection
