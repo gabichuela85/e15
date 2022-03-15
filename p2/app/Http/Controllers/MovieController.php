ab4cff7b7cf7ace048eac375b1ba3ae6a1504e28
Gaby Brown <gaby.leonguerrero@gmail.com>
2022-03-15 03:28:31 +0000

    attempt to add id to each entry in json file

--- p2/app/Http/Controllers/MovieController.php
+++ p2/app/Http/Controllers/MovieController.php
@@ -1,21 +1,40 @@
 <?php
 
-namespace App\Http\Controllers; 
+namespace App\Http\Controllers;
 
-use Illuminate\Support\Arr; 
-use Illuminate\Http\Request; 
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index()
     {
-        $movieList = file_get_contents(database_path('movies.json')); 
-        $movies = json_decode($movieList, true); 
+        $movieList = file_get_contents(database_path('movies.json'));
+        $movies = json_decode($movieList, true);
         
-        $movies = Arr::sort($movies, function($value){
-            return $value['title']; 
-        }); 
+        $movies = Arr::sort($movies, function ($value) {
+            return $value['title'];
+        });
 
-        return view('movies/index', ['movies'=>$movies]); 
+        return view('movies/index', ['movies'=>$movies]);
+    }
+
+    public function form()
+    {
+        $movieList = file_get_contents(database_path('movies.json'));
+        $movies = json_decode($movieList, true);
+        
+        $i = 1;
+
+        foreach ($movies as $movie) {
+            $movie = Arr::add($movie, 'id', $i);
+            $movies = json_encode($movie);
+            $i++;
+        }
+        
+       
+        var_dump($movies);
+        
+        return view('movies/review');
     }
 }
